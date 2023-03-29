a19e326739637a426571e541a048ae7c60502962
Tayanne <tayh.snovaes@gmail.com>
2023-03-29 15:56:02 -0300

    feat: add user data interface

---
+++ src/usecases/register-user/register-user.spec.ts
@@ -0,0 +1,20 @@
+import { UserData } from "./user-data";
+
+describe("Register user on mailing list use case", () => {
+  test("should add user with complete data to mailing list", async () => {
+    const users: UserData[] = [];
+    console.log(users);
+    //   const repo: UserRepository = new InMemoryUserRepository(users);
+    //   const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //     repo,
+    //   );
+
+    //   const name = "Tayanne";
+    //   const email = "tayanne@gmail.com";
+
+    //   const response = await usecase.registerUserOnMailingList({ name, email });
+
+    //   const user = repo.findUserByEmail("tayanne@gmail.com");
+    //   expect((await user).name).toBe("Tayanne");
+  });
+});
